ecdeaf813b271f36516fd82e8fa7b28cbdcfbb44
Tomokazu Watanabe <80309724+tomokazu-watanabe-sti@users.noreply.github.com>
2023-10-06 09:56:32 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,43 @@ import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState('');
   const [incompleteTodos, setIncompleteTodos] = useState([
     'あいうえお',
     'かきくけこ'
   ]);
   const [completeTodos, setCompleteTodos] = useState(['さしすせそ'])
+
+const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+const onClickAdd = () => {
+  if (todoText === "") return;
+  const newTodos = [...incompleteTodos, todoText];
+  setIncompleteTodos(newTodos);
+  setTodoText("");
+};
+
+const onclickDelete = (index) => {
+  const newTodos = [...incompleteTodos];
+  newTodos.splice(index, 1);
+  setIncompleteTodos(newTodos);
+};
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText} />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
           <p className="title">未完了のTODO</p>
             <ul>
-              {incompleteTodos.map((todo) => {
+              {incompleteTodos.map((todo, index) => {
                 return (
                 <div key={todo} className="list-row"> 
                     <li>{todo}</li>
                     <button>完了</button>
-                    <button>削除</button>
+                    <button onClick={() => onclickDelete(index)}>削除</button>
                 </div> 
                 );
               })}
